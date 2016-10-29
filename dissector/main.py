9265103ec224e01e030f39e54bccb2bc21c1ccba
Georgios Nikolaidis <yiorgos@nikolaid.is>
2016-10-29 21:18:16 +0300

    Restructure code.

--- dissector/main.py
+++ dissector/main.py
@@ -17,10 +17,11 @@ import sys
 import shutil
 import math
 import argparse
+import collections
 from p4_hlir.main import HLIR
 
 
-def build_preamble_string(protocol_name):
+def generate_preamble_string(protocol_name):
     return ('\n\n-- Auto generated section\n\n'
             'p4_proto = Proto("%s","%s Protocol")\n'
             'function p4_proto.dissector(buffer,pinfo,tree)\n'
@@ -31,7 +32,7 @@ def build_preamble_string(protocol_name):
                protocol_name.upper()))
 
 
-def build_field_string(field, offset):
+def generate_field_string(field, offset):
     byte_offset = offset / 8
     byte_width = int(math.ceil(field.width / 8.0))
     buffer_string = 'buffer(%i,%i)' % (byte_offset, byte_width)
@@ -56,11 +57,31 @@ def build_field_string(field, offset):
             % (buffer_string, field_string, format_type, format_string))
 
 
-def build_postamble_string(table, value):
+def generate_postamble_string(table, value):
     return ('end\n\n'
             'my_table = DissectorTable.get("%s")\n'
             'my_table:add(%s, p4_proto)\n'
             % (table, value))
+
+
+def generate_dependencies(input_hlir):
+    dependency_dict = collections.defaultdict(list)
+    for parse_state in input_hlir.p4_parse_states.itervalues():
+        current_protocol = parse_state.name.split('parse_')[-1]
+        for branch_value, next_parse_state in parse_state.branch_to.iteritems():
+            next_protocol = next_parse_state.name.split('parse_')[-1]
+            try:
+                branch_field = parse_state.return_statement[1][0].split('.')[1]
+                next_protocol_fields = next_parse_state.latest_extraction.fields
+                protocol_dependencies = (current_protocol,
+                                         branch_field,
+                                         branch_value,
+                                         next_protocol_fields)
+                dependency_dict[next_protocol].append(protocol_dependencies)
+            except (AttributeError, IndexError):
+                pass
+    return dependency_dict
+
 
 # Handle input arguments
 arg_parser = argparse.ArgumentParser(description='Create a Wireshark dissector '
@@ -71,6 +92,7 @@ arg_parser.add_argument('-d', metavar='destination',
                         help='Destination file. If none given, the destination'
                              ' has the form <pwd>/<p4_source>-<protocol>.lua')
 arg_parser.add_argument('-p', metavar="protocol", dest="protocol",
+                        default='all',
                         help='Protocol in P4 source file for which to build a '
                              'dissector. Use instance rather than header name '
                              '(i.e. without a "_t" at the end). If none is '
@@ -84,7 +106,8 @@ absolute_source = os.path.abspath(p4_source)
 
 if args.d is None:
     # Set default filename if the user did not provide one
-    dissector_filename = '%s-%s.lua' % (os.path.basename(p4_source), p4_protocol)
+    dissector_filename = '%s-%s.lua' % (
+    os.path.basename(p4_source), p4_protocol)
 else:
     # Otherwise, check that the destination path exists, and if yes use it
     # for the output
@@ -95,53 +118,39 @@ else:
         print "Error: Destination path (%s) does not exist." % destination_path
         sys.exit()
 
-
 # Build HLIR from input
 input_hlir = HLIR(absolute_source)
 input_hlir.build()
 
-# Get a parser_state
-parse_state = None
-for parse_state_iterator in input_hlir.p4_parse_states.itervalues():
-    # Find the corresponding parser state. "start" doesn't have a
-    # latest_extraction so check for that first
-    if (parse_state_iterator.name != "start"
-            and parse_state_iterator.latest_extraction.name == p4_protocol):
-        parse_state = parse_state_iterator
-        break
-if not parse_state:
-    print "Protocol %s not found in %s" % (p4_protocol, absolute_source)
-    exit()
+# Generate dependencies
+# TODO: Build all dissectors if protocol is 'all'
+dependency_dict = generate_dependencies(input_hlir)
+if p4_protocol in dependency_dict:
+    protocol_name = 'p4_' + p4_protocol
+    previous_protocol, decision_field, decision_value, header_fields = \
+        dependency_dict[p4_protocol][0]
+else:
+    print 'Protocol %s does not exist in %s' % (p4_protocol, p4_source)
+    sys.exit()
 
-# Fields of the parser state
-header_fields = parse_state.latest_extraction.fields
-protocol_name = 'p4_' + parse_state.latest_extraction.name
-previous_protocol_name = parse_state.prev.map.keys()[0].latest_extraction.name
-previous_decision_field = \
-    parse_state.prev.map.keys()[0].return_statement[1][0].split('.')[1]
-previous_decision_value = \
-    parse_state.prev.map.keys()[0].return_statement[2][0][0][0][1]
-
-
-output_string = build_preamble_string(protocol_name)
+output_string = generate_preamble_string(protocol_name)
 
 field_offset = 0  # This is in bits
 for field in header_fields:
-    output_string += build_field_string(field, field_offset)
+    output_string += generate_field_string(field, field_offset)
     field_offset += field.width
 
-
 # Register insertion point
-dissector_table = previous_decision_field.lower()
-insertion_value = str(previous_decision_value).lower()
+dissector_table = decision_field.lower()
+insertion_value = str(decision_value).lower()
 
 # Deal with some common cases
-if previous_protocol_name == 'ipv4' or 'ipv6' and dissector_table == 'protocol':
+if previous_protocol == 'ipv4' or 'ipv6' and dissector_table == 'protocol':
     dissector_table = 'ip.proto'
-if previous_protocol_name == 'tcp' and dissector_table == 'port':
+if previous_protocol == 'tcp' and dissector_table == 'port':
     dissector_table = 'tcp.port'
 
-output_string += build_postamble_string(dissector_table, insertion_value)
+output_string += generate_postamble_string(dissector_table, insertion_value)
 
 # Write to file
 template_path = os.path.dirname(os.path.realpath(__file__))
