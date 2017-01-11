b95df011c4be82d41560fc2b41ecfec0353a1c68
Antonin Bas <antonin@barefootnetworks.com>
2017-01-11 02:26:13 -0800

    Cleanup test file
    Improved style of pytest tests and made syre tests could be run from any
    directory.

--- tests/test_p4_gen.py
+++ tests/test_p4_gen.py
@@ -18,19 +18,19 @@ test_p4_gen
 Tests for `p4_gen` module.
 """
 
-import pytest
 import os
 import filecmp
 import subprocess
 
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
 def call_p4_gen_wireshark(options, exec_path=None):
+    if exec_path is None:
+        exec_path = os.path.join(dir_path, os.pardir, "bin/p4-gen-wireshark")
     try:
-        if exec_path:
-            exec_file = exec_path
-        else:
-            exec_file = os.path.abspath("../bin/p4-gen-wireshark")
-        subprocess.call(str(exec_file) + " " + options, shell=True)
+        subprocess.call([exec_path] + options)
     except Exception as e:
         return e
 
@@ -48,27 +48,22 @@ def test_p4_gen_wireshark_wrong_input():
 def test_p4_gen_wireshark_no_options(tmpdir):
     tmp_dir = tmpdir.mkdir("tmp")
     original_dir = os.path.abspath(os.curdir)
-    base_input = os.path.abspath("../tests/p4_programs/simple_nat.p4")
-    base_output = os.path.abspath("../tests/outputs/simple_nat-ipv4.lua")
-    exec_path = os.path.abspath("../bin/p4-gen-wireshark")
+    base_input = os.path.join(dir_path, "p4_programs/simple_nat.p4")
+    base_output = os.path.join(dir_path, "outputs/simple_nat-ipv4.lua")
+    exec_path = os.path.join(dir_path, os.pardir, "bin/p4-gen-wireshark")
     os.chdir(str(tmp_dir))
-    assert call_p4_gen_wireshark(str(base_input), exec_path=exec_path) is None
-    output = os.path.abspath("simple_nat-ipv4.lua")
-    assert filecmp.cmp(str(base_output),
-                       str(output), shallow=False)
-    os.chdir(str(original_dir))
+    assert call_p4_gen_wireshark([base_input], exec_path=exec_path) is None
+    output = os.path.join(str(tmp_dir), "simple_nat-ipv4.lua")
+    assert filecmp.cmp(base_output, output, shallow=False)
+    os.chdir(original_dir)
 
 
 def test_p4_gen_wireshark_given_output_name_and_protocol(tmpdir):
     tmp_dir = tmpdir.mkdir("tmp")
-    original_dir = os.path.abspath(os.curdir)
-    base_input = os.path.abspath("../tests/p4_programs/heavy_hitter.p4")
-    base_output = os.path.abspath("../tests/outputs/heavy_hitter-tcp.lua")
-    exec_path = os.path.abspath("../bin/p4-gen-wireshark")
-    os.chdir(str(tmp_dir))
-    assert call_p4_gen_wireshark(str(base_input) + " -d foo.lua -p tcp",
-                                 exec_path=exec_path) is None
-    output = os.path.abspath("foo.lua")
-    assert filecmp.cmp(str(base_output),
-                       str(output), shallow=False)
-    os.chdir(str(original_dir))
+    base_input = os.path.join(dir_path, "p4_programs/heavy_hitter.p4")
+    base_output = os.path.join(dir_path, "outputs/heavy_hitter-tcp.lua")
+    exec_path = os.path.join(dir_path, os.pardir, "bin/p4-gen-wireshark")
+    output = os.path.join(str(tmp_dir), "foo.lua")
+    options = [base_input, "-d", output, "-p", "tcp"]
+    assert call_p4_gen_wireshark(options, exec_path=exec_path) is None
+    assert filecmp.cmp(base_output, output, shallow=False)
